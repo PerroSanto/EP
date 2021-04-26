860999cae72e10b847e79a424a4c2b6bdbd20559
Martin Acuña <msguser@gmail.com>
2021-04-26 16:50:17 -0300

    first commit

---
+++ clase_4_punto_1.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('blockbuster', 'root', '', {
+  host: 'localhost',
+  dialect: 'mariadb'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+
+/* si no existe creamos la tabla */
+class Pelicula extends Sequelize.Model {}
+Pelicula.init({
+    nombre_pelicula:Sequelize.STRING,
+    anio_estreno:Sequelize.INTEGER
+}, { sequelize, modelName: 'peliculas', timestamps: false});
+
+
+/* crea una pelicula*/
+sequelize.sync()
+  .then(() => Pelicula.create({
+    nombre_pelicula: 'Terminator',
+    anio_estreno: '1982'
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  });
+
+/* modificamos el anio de estreno*/
+Pelicula.update({ anio_estreno: "1984" }, {
+    where: {
+        nombre_pelicula: 'Terminator'
+    }
+  }).then(() => {
+    console.log("Done");
+  });
+
